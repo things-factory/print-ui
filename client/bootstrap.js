b5c9a45a300939548a7393ea4b96aa7432623640
Hearty Oh <heartyoh@hatiolab.com>
2019-06-18 13:03:17 +0900

    v0.0.10

--- client/bootstrap.js
+++ client/bootstrap.js
@@ -2,7 +2,7 @@ import { store } from '@things-factory/shell'
 import { html } from 'lit-html'
 import { UPDATE_PRINTER } from '@things-factory/print-base'
 
-import { APPEND_FOOTERBAR, TOGGLE_OVERLAY, TOOL_POSITION } from '@things-factory/layout-base'
+import { APPEND_CONTEXT_TOOL, TOGGLE_OVERLAY, TOOL_POSITION } from '@things-factory/layout-base'
 
 import '@material/mwc-icon/mwc-icon'
 
@@ -19,8 +19,8 @@ export default function bootstrap() {
   import('./layouts/print-context-ui')
 
   store.dispatch({
-    type: APPEND_FOOTERBAR,
-    footer: {
+    type: APPEND_CONTEXT_TOOL,
+    tool: {
       position: TOOL_POSITION.FRONT,
       template: html`
         <mwc-icon style="padding: 10px; background-color: #CF4545; color: white;" @click="${toggleOverlayTemplate}"
